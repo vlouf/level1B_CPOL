dfb00abaf69fe13ec775e56cf19f65b40c251481
Valentin Louf <valentin.louf@bom.gov.au>
2017-06-06 15:16:29 +1000

    ZH attenuation revision

--- scripts/processing_codes/atten_codes.py
+++ scripts/processing_codes/atten_codes.py
@@ -38,7 +38,7 @@ def compute_attenuation(kdp, alpha=0.08, dr=0.25):
 
     kdp[:, -40:] = 0  # Removing the last gates because of artifacts created by the SOBEL window.
     kdp[:, :40] = 0  # Removing gates because of artifacts created by the SOBEL window.
-    kdp[kdp < 0] = 0
+    # kdp[kdp < 0] = 0
     kdp[kdp > 5] = 0
 
     atten_specific = alpha*kdp  # Bringi relationship
@@ -112,17 +112,38 @@ def correct_attenuation_zh(radar, refl_name='DBZ', kdp_name='KDP_GG'):
             Attenuation corrected reflectivity.
     """
     r = radar.range['data']
-    refl = deepcopy(radar.fields[refl_name]['data'])
+    refl = radar.fields[refl_name]['data']
     kdp = deepcopy(radar.fields[kdp_name]['data'])
 
     dr = (r[1] - r[0]) / 1000  # km
 
-    atten_spec, atten = compute_attenuation(kdp, alpha=0.08, dr=dr)
+    # atten_spec, atten = compute_attenuation(kdp, alpha=0.08, dr=dr)
+    # Check if KDP is a masked array.
+    if np.ma.isMaskedArray(kdp):
+        kdp = kdp.filled(0)  # 0 is the neutral value for a sum
+    kdp[np.isnan(kdp)] = 0
+
+    kdp[:, -40:] = 0  # Removing the last gates because of artifacts created by the SOBEL window.
+    kdp[:, :40] = 0  # Removing gates because of artifacts created by the SOBEL window.
+    kdp[kdp > 5] = 0
+
+    atten_specific = kdp  # Bringi relationship
+    atten_specific[refl < 25] = 0
+    atten_specific[(refl >= 30) & (refl < 40)] *= 0.10
+    atten_specific[(refl >= 40) & (refl < 50)] *= 0.09
+    atten_specific[(refl >= 50)] *= 0.07
+
+    # Path integrated attenuation
+    atten = 2 * np.cumsum(atten_specific, axis=1) * dr
+
+    if atten.max() > 10:
+        print("WARNING: be carfull, risk of overestimating attenuation.")
+        print("Atten max is %f dB." % (atten.max()))
 
     zh_meta = pyart.config.get_metadata('corrected_reflectivity')
     zh_meta['data'] = refl + atten
 
-    atten_meta = {'data': atten_spec, 'units': 'dB/km', 'standard_name': 'specific_attenuation_zh',
+    atten_meta = {'data': atten_specific, 'units': 'dB/km', 'standard_name': 'specific_attenuation_zh',
                   'long_name': 'Reflectivity specific attenuation'}
 
     return atten_meta, zh_meta
